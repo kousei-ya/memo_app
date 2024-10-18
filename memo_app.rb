147cc1d77a38dfdedd1c2f803f5b1f421c6e49fa
kousei-ya <k.yano@genbasupport.com>
2024-10-18 15:52:19 +0900

    メモアプリデータベース版を作成

--- memo_app.rb
+++ memo_app.rb
@@ -2,7 +2,7 @@
 
 require 'sinatra'
 require 'sinatra/reloader'
-require 'json'
+require 'pg'
 
 helpers do
   def h(text)
@@ -10,12 +10,38 @@ helpers do
   end
 end
 
-def load_memos
-  File.exist?('memo.json') ? JSON.parse(File.read('memo.json')) : {}
+def conn
+  @conn ||= PG.connect(dbname: 'memodb')
 end
 
-def save_memos(memos)
-  File.write('memo.json', JSON.generate(memos))
+configure do
+  result = conn.exec("SELECT * FROM information_schema.tables WHERE table_name = 'memos'")
+  conn.exec('CREATE TABLE memos (id serial, title varchar(255), content text)') if result.values.empty?
+end
+
+def load_memos
+  result = conn.exec('SELECT * FROM memos')
+  memos = {}
+  result.each do |row|
+    memos[row['id']] = {
+      'id' => row['id'].force_encoding('UTF-8'),
+      'title' => row['title'].force_encoding('UTF-8'),
+      'content' => row['content'].force_encoding('UTF-8')
+    }
+  end
+  memos
+end
+
+def create_memo(title, content)
+  conn.exec_params('INSERT INTO memos(title, content) VALUES ($1, $2);', [title, content])
+end
+
+def edit_memo(title, content, id)
+  conn.exec_params('UPDATE memos SET title = $1, content = $2 WHERE id = $3', [title, content, id])
+end
+
+def delete_memo(id)
+  conn.exec_params('DELETE FROM memos WHERE id = $1', [id])
 end
 
 not_found do
@@ -30,58 +56,62 @@ get '/memos' do
   @memos = load_memos
   erb :index
 end
+
 get '/memos/new' do
   erb :new
 end
 
 get '/memos/:id' do
   @id = params[:id]
-  @memo = load_memos[@id]
+  memos = load_memos
+  @memo = memos[@id]
   if @memo
     erb :show
   else
     status 404
-    erb :error_404
+    erb :error_not_found
   end
 end
 
 post '/memos' do
-  memos = load_memos
-  id = SecureRandom.uuid
-  memos[id] = { 'title' => params[:title], 'content' => params[:content] }
-  save_memos(memos)
+  title = params[:title]
+  content = params[:content]
+  create_memo(title, content)
   redirect '/memos'
 end
 
 get '/memos/:id/edit' do
   @id = params[:id]
-  @memo = load_memos[@id]
+  memos = load_memos
+  @memo = memos[@id]
   if @memo
+    @title = @memo['title']
+    @content = @memo['content']
     erb :edit
   else
     status 404
-    erb :error_404
+    erb :error_not_found
   end
 end
 
 patch '/memos/:id' do
   memos = load_memos
-  id = params[:id]
-  @memo = memos[id]
+  @id = params[:id]
+  @memo = memos[@id]
   if @memo
-    memos[id]['title'] = params[:title]
-    memos[id]['content'] = params[:content]
-    save_memos(memos)
+    title = params[:title]
+    content = params[:content]
+    edit_memo(title, content, @id)
     redirect '/memos'
   else
     status 404
-    erb :error_404
+    erb :error_not_found
   end
 end
 
 delete '/memos/:id' do
   memos = load_memos
-  memos.delete(params[:id])
-  save_memos(memos)
+  @id = params[:id]
+  delete_memo(@id)
   redirect '/memos'
 end
